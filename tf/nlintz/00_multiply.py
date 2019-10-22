e8c18f5aa53a48e2e0bd3b1e7e80ba1e54612612
jchwenger <34098722+jchwenger@users.noreply.github.com>
2019-10-22 10:47:31 +0100

    TF nlintz moving data to new dir

--- tf/nlintz/00_multiply.py
+++ tf/nlintz/00_multiply.py
@@ -2,11 +2,16 @@
 
 import tensorflow as tf
 
-a = tf.placeholder("float") # Create a symbolic variable 'a'
-b = tf.placeholder("float") # Create a symbolic variable 'b'
+a = tf.placeholder(tf.float32) # Create a symbolic variable 'a'
+b = tf.placeholder(tf.float32) # Create a symbolic variable 'b'
 
 y = tf.multiply(a, b) # multiply the symbolic variables
+z = tf.pow(a, b)
+x = tf.exp(a)
 
 with tf.Session() as sess: # create a session to evaluate the symbolic expressions
-    print("%f should equal 2.0" % sess.run(y, feed_dict={a: 1, b: 2})) # eval expressions with parameters for a and b
-    print("%f should equal 9.0" % sess.run(y, feed_dict={a: 3, b: 3}))
+    print("Multiplication, TF variable: %f should equal 2.0" % sess.run(y, feed_dict={a: 1, b: 2})) # eval expressions with parameters for a and b
+    print("Multiplication, TF variable: %f should equal 9.0" % sess.run(y, feed_dict={a: 3, b: 3}))
+    print("a^b power, TF variable: %f should equal 4.0" % sess.run(z, feed_dict={a: 2, b: 2})) # eval expressions with parameters for a and b
+    print("a^b power, TF variable: %f should equal 8.0" % sess.run(z, feed_dict={a: 2, b: 3}))
+    print("Exponentiation e^x, TF variable: %f should equal 7.389056" % sess.run(x, feed_dict={a: 2.0}))
--- tf/nlintz/04_modern_net.py
+++ tf/nlintz/04_modern_net.py
@@ -2,41 +2,53 @@
 
 import tensorflow as tf
 import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
 
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+trX = tf.reshape(x_train, [-1, 784])
+trY = tf.one_hot(y_train, 10)
+teX = tf.reshape(x_test, [-1, 784])
+teY = tf.one_hot(y_test, 10)
 
 def init_weights(shape):
     return tf.Variable(tf.random_normal(shape, stddev=0.01))
 
+def init_dropout():
+    return tf.Variable("float")
 
-def model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden): # this network is the same as the previous one except with an extra hidden layer + dropout
-    X = tf.nn.dropout(X, p_keep_input)
-    h = tf.nn.relu(tf.matmul(X, w_h))
+def model(X, weights, dropout): 
 
-    h = tf.nn.dropout(h, p_keep_hidden)
-    h2 = tf.nn.relu(tf.matmul(h, w_h2))
+    current_input = X
+    current_input = tf.nn.dropout(X, dropout[0])
 
-    h2 = tf.nn.dropout(h2, p_keep_hidden)
+    for i in range(len(weights[:-1])):
+        h = tf.nn.relu(tf.matmul(current_input, weights[i]))
+        h = tf.nn.dropout(h, dropout[1])
+        current_input = h
 
-    return tf.matmul(h2, w_o)
-
-
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+    return tf.matmul(current_input, weights[-1])
 
 X = tf.placeholder("float", [None, 784])
 Y = tf.placeholder("float", [None, 10])
 
-w_h = init_weights([784, 625])
-w_h2 = init_weights([625, 625])
-w_o = init_weights([625, 10])
+layers = [784, 128, 64, 64, 64, 128, 10]
 
-p_keep_input = tf.placeholder("float")
-p_keep_hidden = tf.placeholder("float")
-py_x = model(X, w_h, w_h2, w_o, p_keep_input, p_keep_hidden)
+weights = []
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
-train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+for i in range(len(layers)-1):
+    weight_i = init_weights([layers[i], layers[i+1]])
+    weights.append(weight_i)
+
+dropout = tf.placeholder("float", shape=(2))
+
+py_x = model(X, weights, dropout)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=py_x, labels=Y))
+# train_op = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
+train_op = tf.train.AdamOptimizer(0.001).minimize(cost)
 predict_op = tf.argmax(py_x, 1)
 
 # Launch the graph in a session
@@ -44,11 +56,28 @@ with tf.Session() as sess:
     # you need to initialize all variables
     tf.global_variables_initializer().run()
 
-    for i in range(100):
+    trX = trX.eval()
+    trY = trY.eval()
+    teX = teX.eval()
+    teY = teY.eval()
+
+    for i in range(20):
         for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
-            sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end],
-                                          p_keep_input: 0.8, p_keep_hidden: 0.5})
-        print(i, np.mean(np.argmax(teY, axis=1) ==
-                         sess.run(predict_op, feed_dict={X: teX, 
-                                                         p_keep_input: 1.0,
-                                                         p_keep_hidden: 1.0})))
+
+            sess.run(train_op, feed_dict={X: trX[start:end], 
+                                          Y: trY[start:end],
+                                          dropout: [0.8, 0.5]})
+
+        if i % 2 == 0:
+            cost_i = sess.run(cost, feed_dict={X: teX,
+                                               Y: teY,
+                                               dropout: [0.8, 0.5]}) 
+
+            acc_i = np.mean(np.argmax(teY, axis=1) ==
+                           sess.run(predict_op, feed_dict={X: teX, 
+                                                           Y: teY,
+                                                           dropout: [1.0, 1.0]}))
+
+            print('Iterations:', i, 
+                  '| Cost:', cost_i,
+                  '| Accuracy:', acc_i)
